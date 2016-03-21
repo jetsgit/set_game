eaef25eb344c53aaf68fa81c147dd17ea4d524aa
Jerrold Thompson <jet@whidbey.com>
2016-03-21 09:05:10 -0700

    Adding colorized minitest-reporters

--- Gemfile
+++ Gemfile
@@ -5,4 +5,5 @@ group :test do
   gem 'pry'
   gem 'awesome_print'
   gem 'minitest', '~>5.8.0'
+  gem 'minitest-reporters', '1.0.5'
 end
--- test/spec_helper.rb
+++ test/spec_helper.rb
@@ -4,3 +4,47 @@ require File.expand_path('../../lib/play_set', __FILE__)
 
 require "minitest/autorun"
 require "minitest/spec"
+require "minitest/reporters"
+
+
+module Minitest
+  module Reporters
+    class AwesomeReporter < DefaultReporter
+      GRAY = '0;36'
+      GREEN = '1;32'
+      RED = '1;31'
+
+      def initialize(options = {})
+        super
+        @slow_threshold = options.fetch(:slow_threshold, nil)
+      end
+
+      def record_pass(test)
+        if @slow_threshold.nil? || test.time <= @slow_threshold
+          super
+        else
+          gray('O')
+        end
+      end
+
+      def color_up(string, color)
+        color? ? "\e\[#{ color }m#{ string }#{ ANSI::Code::ENDCODE }" : string
+      end
+
+      def red(string)
+        color_up(string, RED)
+      end
+
+      def green(string)
+        color_up(string, GREEN)
+      end
+
+      def gray(string)
+        color_up(string, GRAY)
+      end
+    end
+  end
+end
+
+reporter_options = { color: true, slow_count: 5 }
+Minitest::Reporters.use! [Minitest::Reporters::AwesomeReporter.new(reporter_options)]
